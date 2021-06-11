a29cddac057d71d319731955241eac02a13a7b5e
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-06-11 18:10:06 +0530

    Add files via upload

---
+++ src/com/techlab/test/ExceptionTest.java
@@ -0,0 +1,10 @@
+package com.techlab.test;
+
+public class ExceptionTest {
+
+	public static void main(String[] args) {
+//		System.out.println("Inside Main");
+		main(new String[]{"Hello", "Main"});
+	}
+
+}
